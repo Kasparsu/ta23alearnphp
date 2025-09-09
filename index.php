422fad02186dfd6aba7e10e8dccb0d02dd82057b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-09-09 10:19:45 +0300

    Add boolean math example

--- index.php
+++ index.php
@@ -1,42 +1,18 @@
 <?php
-$array = array(1, 2, 3, 4);
-$array = [1, 2, 3, 4];
-$array = [
-    1,
-    'hello',
-    true,
-    NULL,
-    [1, 2, 3]
-];
-$array = [
-    'name' => 'Kaspar',
-    'age' => 32,
-    'gender' => 'Apache helicopter',
-    'isCool' => true
-]; 
-$array = [
-    6 => 'lol',
-    1,
-    2,
-    3,
-    'name' => 'Kaspar',
-    'age' => 32,
-    1000 => 88,
-    'hello world',
-    'gender' => 'Apache helicopter',
-    'isCool' => true,
-    4,
-    5,
-]; 
-var_dump($array[7]);
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
-$array = [1,2,3];
-array_push($array, 4);
-array_push($array, 5, 6, 7);
-$array[] = 8;
-var_dump($array);
+
+$bool = true && false;
+$bool = true || false;
+$bool = !false;
+$bool = (true && false || false) && !(true || false && true);
+$bool = 10 > 5;
+$bool = 10 < 5;
+$bool = 10 > 10;
+$bool = 10 < 10;
+$bool = 10 == 10;
+$bool = 10 >= 10;
+$bool = 10 <= 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 !== '10';
+$bool = 10 != '10';
+var_dump($bool);
